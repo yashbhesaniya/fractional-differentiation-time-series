b13685557c7bde5ac3d9e6902e9209dca8663bef
Preet Vadaliya <preetvadaliya@gmail.com>
2024-01-11 16:58:16 +0100

    change: required changes to run

--- examples/frac_diff_sp500.py
+++ examples/frac_diff_sp500.py
@@ -2,9 +2,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from fracdiff2 import frac_diff_ffd
+# from fracdiff2 import frac_diff_ffd
 from utils import plot_multi
 
+def _get_weight_ffd(d, thres, lim):
+    w, k = [1.], 1
+    ctr = 0
+    while True:
+        w_ = -w[-1] / k * (d - k + 1)
+        if abs(w_) < thres:
+            break
+        w.append(w_)
+        k += 1
+        ctr += 1
+        if ctr == lim - 1:
+            break
+    w = np.array(w[::-1]).reshape(-1, 1)
+    return w
+
+
+def frac_diff_ffd(x, d, thres=1e-5, disable_warning=False):
+    if np.max(x) > 10.0 and not disable_warning:
+        print('WARNING: have you applied log before calling this function? If yes, discard this warning.')
+    w = _get_weight_ffd(d, thres, len(x))
+    width = len(w) - 1
+    output = []
+    output.extend([0] * width)
+    for i in range(width, len(x)):
+        output.append(np.dot(w.T, x[i - width:i + 1])[0])
+    return np.array(output)
 
 def main():
     df = pd.read_csv('../doc/sp500.csv', index_col=0, parse_dates=True)
